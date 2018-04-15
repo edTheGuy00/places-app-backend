bbcd8f3263a35c2ce83a0dfa0ba24b0a0775a0be
edgar <edgrr.trem000@gmail.com>
2018-04-14 22:15:21 -0500

    restructure directory

--- models/index.js
+++
@@ -1,9 +0,0 @@
-import Sequelize from 'sequelize';
-
-const sequelize = new Sequelize('placesapp', 'postgres', 'postgres', {
-  dialect: 'postgres',
-  operatorsAliases: Sequelize.Op,
-  define: {
-    underscored: true,
-  },
-});
---
+++ src/models/index.js
@@ -0,0 +1,25 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('placesapp', 'postgres', 'postgres', {
+  dialect: 'postgres',
+  operatorsAliases: Sequelize.Op,
+  define: {
+    underscored: true,
+  },
+});
+
+const models = {
+  User: sequelize.import('./user'),
+  Place: sequelize.import('./place'),
+};
+
+Object.keys(models).forEach((modelName) => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+export default models;
